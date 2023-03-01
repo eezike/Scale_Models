10879a8ff9c26110388f9b22364073c4ef1cc8f3
Your Name <victorgoncalves@college.harvard.edu>
2023-03-01 17:46:50 -0500

    communication nearly done. Have not tested the code so prone to failure.

--- machine.py
+++ machine.py
@@ -4,6 +4,7 @@ import socket
 from configuration import Configuration
 import threading
 import time
+import random
 from  logical_clock import LogicalClock
 
 class Machine:
@@ -12,13 +13,18 @@ class Machine:
         self.config = Configuration(machine_id)
         self.clock = LogicalClock()
         self.queue = []
-        self.log_file = open(f"machine{machine_id}.log", "w")
-        self.stop_event = threading.Event()
+
+        log_dir = "/logs"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        self.log_file = open(f"{log_dir}/machine{machine_id}.log", "a+")
 
         self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.send_sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM), socket.socket(socket.AF_INET, socket.SOCK_STREAM)]
 
         self.receive_thread = threading.Thread(target=self.receive_loop, daemon=True)
+        self.send_thread = threading.Thread(target=self.send_loop, daemon=True)
+        self.stop_event = threading.Event()
         self.lock = threading.Lock()
 
         self.start()
@@ -27,6 +33,7 @@ class Machine:
     def start(self):
         self.receive_thread.start()
         self.connect()
+        self.send_thread.start()
 
     # Stop machine
     def stop(self):
@@ -47,13 +54,9 @@ class Machine:
                 except socket.error as msg:
                     print(f"Socket binding error: " + str(msg) + "\n" + "Retrying in 15 secs...")
                     time.sleep(15)
-            
-            # Once connected, switch to sending messages
-            print("Connected to machine at: " + str(port))
-            self.send_loop(self.send_sockets[sock_idx])
 
-        # Connect to each machine in a separate thread
         for i, peer_port in enumerate(self.config.peer_ports):
+            # Connect to each machine in a separate thread
             threading.Thread(target = connect_loop, args=(peer_port, i)).start()
 
     # Start receiving and handling clients
@@ -73,16 +76,51 @@ class Machine:
 
     def handle_client(self, clientsocket: socket.socket):
         while not self.stop_event.is_set():
-            # TODO: receive messages from network queue and update logical clock
-            pass
+            # Receive messages and put them into the queue
+            try:
+                # Receive the message from the client
+                message = clientsocket.recv(1024).decode('utf-8')
+                if not message:
+                    break
+                
+                # Add the message to the queue
+                self.queue.append(message)
+            
+            except socket.error as msg:
+                # Handle case when the client abruptly terminates the connection
+                print(msg)
     
-    def send_loop(self, ):
+        # Clean up the client socket
+        clientsocket.close()
+    
+    def send_loop(self):
+        # Send messages to other machines with random probabilities
         while not self.stop_event.is_set():
-            # TODO: send messages to other machines with random probabilities
-            pass    
 
-    def log_event(self, event_type, timestamp):
-        # TODO: log event to file with machine id, event type, timestamp, queue length, and logical clock value
+            # Choose a message event between 1-10
+            event = random.randint(1, 11)
+            message = self.clock.time # TODO: we probably want to send the machine ID here too
+
+            """
+            Event 1: Send the message to a machine
+            Event 2: Send the message to the other machine (not from event 1)
+            Event 3: Send the message to both of the other machines
+            Events 4-10: Internal clock updates
+            """
+            if event == 1:
+                self.send_sockets[0].sendall(message.encode())
+            elif event == 2:
+                self.send_sockets[1].sendall(message.encode())
+            elif event == 3:
+                for sock in self.send_sockets:
+                    sock.sendall.sendall(message.encode())
+            else:
+                pass
+
+            self.log_event(event)
+
+    def log_event(self, event_type):
+        # Log event to file with machine id, event type, timestamp, queue length, and logical clock value
         queue_length = len(self.queue)
         system_time = time.time()
         log_entry = f"[{system_time}] Machine {self.machine_id} received an {event_type} message, queue length: {queue_length}, logical clock time: {self.clock.time}\n"
