c1653ffe46ae87ebe305b6a9afbe3a083795414b
Your Name <victorgoncalves@college.harvard.edu>
2023-03-01 23:58:37 -0500

    refactored code

--- machine.py
+++ machine.py
@@ -6,14 +6,21 @@ import threading
 import time
 import random
 from  logical_clock import LogicalClock
-
+        
 class Machine:
+    HOST = "localhost"
+    NUM_PROCESSES = 3
+
     def __init__(self, machine_id) -> None:
-        self.machine_id = machine_id
-        self.config = Configuration(machine_id)
+        self.machine_id = int(machine_id)
+        
+        # Initialize the machine's clock
         self.clock = LogicalClock()
+        self.clock_rate = random.randint(1, 6)
+        print(f"{self.machine_id}'s clock rate: {self.clock_rate}")
         self.queue = []
 
+        # Create the log file
         log_dir = "/logs"
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
@@ -22,11 +29,27 @@ class Machine:
         self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.send_sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM), socket.socket(socket.AF_INET, socket.SOCK_STREAM)]
 
+        # Define the machine's port and the port of other processes
+        if self.machine_id == 1:
+            self.port = 8000
+            self.peer_ports = [8001, 8002]
+        elif self.machine_id == 2:
+            self.port = 8001
+            self.peer_ports = [8000, 8002]
+        elif self.machine_id == 3:
+            self.port = 8002
+            self.peer_ports = [8000, 8001]
+        else:
+            raise ValueError("Invalid process ID")
+        
+        # Keep track of connected sockets 
+        self.connected = 0
+
+        # Define the threads for receiving and sending messages
         self.receive_thread = threading.Thread(target=self.receive_loop, daemon=True)
         self.send_thread = threading.Thread(target=self.send_loop, daemon=True)
         self.stop_event = threading.Event()
         self.lock = threading.Lock()
-
         self.start()
 
     # Start machine
@@ -41,7 +64,7 @@ class Machine:
 
     # Connect to all other machines
     def connect(self):
-        if len(self.send_sockets) != len(self.config.peer_ports):
+        if len(self.send_sockets) != len(self.peer_ports):
             print("Peer ports and send sockets do not match")
             os._exit(1)
 
@@ -49,21 +72,21 @@ class Machine:
             # Try connecting every 15 seconds
             while True:
                 try:
-                    self.send_sockets[sock_idx].connect((self.config.HOST, port))
+                    self.send_sockets[sock_idx].connect((self.HOST, port))
                     break
                 except socket.error as msg:
-                    print(f"Socket binding error: " + str(msg) + "\n" + "Retrying in 15 secs...")
-                    time.sleep(15)
+                    print(f"Socket binding error: " + str(msg) + "\n" + "Retrying in 30 secs...")
+                    time.sleep(30)
 
-        for i, peer_port in enumerate(self.config.peer_ports):
+        for i, peer_port in enumerate(self.peer_ports):
             # Connect to each machine in a separate thread
             threading.Thread(target = connect_loop, args=(peer_port, i)).start()
 
     # Start receiving and handling clients
     def receive_loop(self):
 
-        self.receive_socket.bind((self.config.HOST, self.config.port))
-        self.receive_socket.listen(self.config.NUM_PROCESSES - 1)
+        self.receive_socket.bind((self.HOST, self.port))
+        self.receive_socket.listen(self.NUM_PROCESSES - 1)
 
         clientsockets = []
         
@@ -72,14 +95,14 @@ class Machine:
             clientsocket, addr = self.receive_socket.accept()
             print(f"{self.machine_id}: {addr[0]} has joined")
             clientsockets.append(clientsocket)
-            threading.Thread(target = self.handle_client, args = (clientsocket, addr)).start()
+            threading.Thread(target = self.handle_client, args = (clientsocket,)).start()
 
-    def handle_client(self, clientsocket: socket.socket):
+    def handle_client(self, clientsocket):
         while not self.stop_event.is_set():
             # Receive messages and put them into the queue
             try:
                 # Receive the message from the client
-                message = clientsocket.recv(1024).decode('utf-8')
+                message = clientsocket.recv(2048).decode('utf-8')
                 if not message:
                     break
                 
@@ -96,10 +119,12 @@ class Machine:
     def send_loop(self):
         # Send messages to other machines with random probabilities
         while not self.stop_event.is_set():
+            if self.connected != self.NUM_PROCESSES - 1:
+                pass
 
             # Choose a message event between 1-10
             event = random.randint(1, 11)
-            message = self.clock.time # TODO: we probably want to send the machine ID here too
+            message = str(self.clock.time) # TODO: we probably want to send the machine ID here too
 
             """
             Event 1: Send the message to a machine
@@ -113,7 +138,7 @@ class Machine:
                 self.send_sockets[1].sendall(message.encode())
             elif event == 3:
                 for sock in self.send_sockets:
-                    sock.sendall.sendall(message.encode())
+                    sock.sendall(message.encode())
             else:
                 pass
 
