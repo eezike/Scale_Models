c3b86b00955723aa29f97b082a52987582fc8eb3
Your Name <victorgoncalves@college.harvard.edu>
2023-03-08 13:08:52 -0500

    All unit tests are working

--- unit_tests.py
+++ unit_tests.py
@@ -63,6 +63,7 @@ class TestMachine(unittest.TestCase):
         print("testInitialization passed")
 
     def testConnection(self) -> None:
+        
         # Init machines
         self.machine1 = Machine(1, True)
         self.machine2 = Machine(2, True)
@@ -107,11 +108,21 @@ class TestMachine(unittest.TestCase):
         self.machine2.CONNECTION_WAIT = 0
         self.machine3.CONNECTION_WAIT = 0
 
+        # Ensure machines are not connected at first
+        self.assertFalse(self.machine1.receive_connected or self.machine1.send_connected)
+        self.assertFalse(self.machine2.receive_connected or self.machine2.send_connected)
+        self.assertFalse(self.machine3.receive_connected or self.machine3.send_connected)
+
         # Connect all the machines (wait 1 secs to ensure they have connected)
         threading.Thread(target = self.machine1.connect).start()
         threading.Thread(target = self.machine2.connect).start()
         threading.Thread(target = self.machine3.connect).start()
         time.sleep(1)
+
+        # Ensure machines have connected
+        self.assertTrue(self.machine1.receive_connected and self.machine1.send_connected)
+        self.assertTrue(self.machine2.receive_connected and self.machine2.send_connected)
+        self.assertTrue(self.machine3.receive_connected and self.machine3.send_connected)
 
         message =  struct.pack("i", 1000)
 
@@ -122,18 +133,21 @@ class TestMachine(unittest.TestCase):
 
         # Test event 1: Machine 1 sends to Machine 2
         self.machine1.send_sockets[self.machine1.PEER_PORTS[0]].sendall(message)
-        # self.assertEqual(self.machine2.message_queue.qsize(), 1)
+        time.sleep(.05) # Need to sleep before checking queue to ensure message arrived
+        self.assertEqual(self.machine2.message_queue.qsize(), 1)
             
         # Test event 2: Machine 1 sends to Machine 3
         self.machine1.send_sockets[self.machine1.PEER_PORTS[1]].sendall(message)
-        # self.assertEqual(self.machine3.message_queue.qsize(), 1)
+        time.sleep(.05)
+        self.assertEqual(self.machine3.message_queue.qsize(), 1)
             
         # Test event 3: Machine 1 sends to both
         for sock in self.machine1.send_sockets.values():
             sock.sendall(message)
+        time.sleep(.05)
         
-        # self.assertEqual(self.machine2.message_queue.qsize(), 2)
-        # self.assertEqual(self.machine3.message_queue.qsize(), 2)
+        self.assertEqual(self.machine2.message_queue.qsize(), 2)
+        self.assertEqual(self.machine3.message_queue.qsize(), 2)
 
         # Close all the machines
         self.machine1.close()
@@ -205,8 +219,6 @@ class TestLogicalClocks(unittest.TestCase):
         self.assertEqual(self.clock.get_time(), 10001)
 
         print("testClockUpdate")
-
-
 
 if __name__ == '__main__':
     print("Begining unit tests...")
